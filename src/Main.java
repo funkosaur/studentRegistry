d81440dcd680ea8af7bc90ca7ad91eb0a563e401
funkosaur <mario.bojkovski@outlook.com>
2023-12-27 02:07:09 +0000

    refactored code in university register, changed the student constructor into a record

--- src/Main.java
+++ src/Main.java
@@ -6,10 +6,10 @@ public class Main {
     public static void main(String[] args) {
         UniversityRegister register = new UniversityRegister();
 
-        register.addStudent("John Smith", 1, "Computer Science", "CS101");
-        register.addStudent("Alice Johnson", 2, "Mathematics", "Math101");
-        register.addStudent("Bob Brown", 3, "Computer Science", "CS102");
-        register.addStudent("Eve Davis", 4, "Physics", "PHY101");
+        register.addStudent("John Smith",  "Computer Science", "CS101", 66);
+        register.addStudent("Alice Johnson",  "Mathematics", "Math101", 45);
+        register.addStudent("Bob Brown",  "Computer Science", "CS101",88);
+        register.addStudent("Eve Davis",  "Physics", "PHY101",77);
 
         // Query students by name
         List<Student> johns = register.queryStudentsByName("John Smith");
@@ -17,22 +17,23 @@ public class Main {
         UniversityRegister.displayStudents(johns);
 
         // Query students by course and name
-        List<Student> csJohns = register.queryStudentsByCourse("Computer Science", "John");
+        List<Student> csJohns = register.queryStudentsByCourseAndName("Computer Science", "John");
         System.out.println("Computer Science students with 'John' in their name: ");
-        for (Student student : csJohns) {
-            System.out.println("Course: " + student.getCourse());
-        }
+        UniversityRegister.displayStudents(csJohns);
 
         // Remove a student by ID
         register.removeStudent(4);
 
         // Get students whose names start with 'A'
-        List<Student> aStudents = register.queryStudentsByName("Alice Johnson");
+        List<Student> aStudents = register.queryStudentsByName("Eve Davis");
         System.out.println("Students named Alice: ");
         for (Student student : aStudents) {
-            System.out.println("Name: " + student.getName());
+            System.out.println("Name: " + student.name());
         }
-        register.addStudent("cock", 6, "comp", "science");
+        register.addStudent("cgfdh",  "comp", "science", 78);
+
+        List<Student> csJohns1 = register.getAllStudentsOnModuleSortedByMarks("CS101");
+        UniversityRegister.displayStudents(csJohns1);
 
     }
 }
--- src/UniversityRegister.java
+++ src/UniversityRegister.java
@@ -1,88 +1,107 @@
 import java.util.*;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 public class UniversityRegister {
     private final ArrayList<Student> students;
 
+
     public UniversityRegister() {
         students = new ArrayList<>();
     }
-    
+
     static void displayStudents(List<Student> students) {
         for (Student student : students) {
-            System.out.println("Name: " + student.getName());
-            System.out.println("ID: " + student.getId());
-            System.out.println("Course: " + student.getCourse());
-            System.out.println("Module: " + student.getModule());
+            System.out.println("Name: " + student.name());
+            System.out.println("ID: " + student.id());
+            System.out.println("Course: " + student.course());
+            System.out.println("Module: " + student.module());
+            System.out.println("Mark: " + student.mark());
             System.out.println();
         }
     }
 
-    public void addStudent(String name, int id, String course, String module) {
-        Student studentWithSameId = findStudentWithSameId(id);
-        if (studentWithSameId != null) {
-            String errorMessage = "A student with ID " + id + " already exists.";
-            throw new IllegalArgumentException(errorMessage);
+    private static int idCounter = 0;
+    public void addStudent(String name, String course, String module, int mark) {
+        if (name == null || course == null || module == null) {
+            throw new IllegalArgumentException("Arguments cannot be null.");
         }
-
-        Student newStudent = new Student(name, id, course, module);
-        System.out.println(newStudent.getName());
+        int id = ++idCounter;
+        Student newStudent = new Student(name, id, course, module, mark);
         students.add(newStudent);
     }
 
     private Student findStudentWithSameId(int id) {
         return students.stream()
-                .filter(student -> student.getId() == id)
+                .filter(student -> student.id() == id)
                 .findFirst()
                 .orElse(null);
     }
 
     public void removeStudent(int id) {
-        boolean wasRemoved = students.removeIf(student -> student.getId() == id);
+        boolean wasRemoved = students.removeIf(student -> student.id() == id);
         if(!wasRemoved){
             System.out.println("No student found with ID: " + id);
-        } else {
-            students.removeIf(student -> student.getId() == id);
-            System.out.println(students);
-            displayStudents(students);
         }
     }
+
+    private List<Student> _queryStudents(Predicate<Student> predicate) {
+        return students.stream()
+                .filter(predicate)
+                .collect(Collectors.toList());
+    }
+
+    public List<Student> queryStudentsByName(String name) {
+        return _queryStudents(student -> student.name().equals(name));
+    }
+
+
+
+    /*
 
     public List<Student> queryStudentsByName(String name) {
 
         return students.stream()
                 .filter(student -> student.getName().equals(name))
                 .collect(Collectors.toList());
+    }*/
+
+    public List<Student> queryStudentsByCourse(String course) {
+        return _queryStudents(student -> student.course().equals(course));
     }
 
-    public List<Student> queryStudentsByCourse(String course, String searchText) {
-        return students.stream()
-                .filter(student -> student.getCourse().equals(course) && student.getName().contains(searchText))
-                .collect(Collectors.toList());
+    public List<Student> queryStudentsByCourseAndName(String course, String searchText) {
+        return _queryStudents(student -> student.course().equals(course) && student.name().contains(searchText));
     }
 
     public List<Student> queryStudentsByModule(String module) {
+        return _queryStudents(student -> student.module().equals(module));
+    }
+
+    /* public List<Student> queryStudentsByModule(String module) {
         return students.stream()
                 .filter(student -> student.getModule().equals(module))
                 .collect(Collectors.toList());
-    }
+    }*/
+
+
 
     public List<Student> getAllStudentsOnModuleSortedByMarks(String module) {
-        // Implement this function to sort students by marks, if you have a marks attribute in the Student class.
-        // You can use functional programming constructs to achieve this.
-        return null;
-    }
-
-    public List<Student> getStudentsByCourseAndName(String course, String searchText) {
-        // Implement this function to filter students by course and name using functional programming.
-        return null;
+        return students.stream()
+                .filter(student -> student.module().equals(module))
+                .sorted(Comparator.comparing(Student::mark).reversed())
+                .collect(Collectors.toList());
     }
 
     public List<Student> getStudentsWhoseNamesStartWith(char letter) {
-        // Implement this function to filter students whose names start with a given letter using functional programming.
-        return null;
+        return _queryStudents(student -> student.name().startsWith(String.valueOf(letter)));
     }
 
-
+    public OptionalDouble calculateAverageMarksForCourse(String course) {
+        return students.stream()
+                .filter(student -> student.course().equals(course))
+                .mapToInt(Student::mark)
+                .average();
+    }
 
 }
